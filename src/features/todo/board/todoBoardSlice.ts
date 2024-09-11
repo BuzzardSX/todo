d17a5cf007ed84c58a3c13fd40754187e50cce1c
Павел Бобров <buzzard.sx@gmail.com>
2024-09-11 23:16:31 +0500

    values

--- src/features/todo/board/todoBoardSlice.ts
+++ src/features/todo/board/todoBoardSlice.ts
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const todoBoardSlice = createSlice({
 	name: 'board',
 	initialState: {
-		lists: [1, 2, 3]
+		lists: [4, 5, 6]
 	},
 	reducers: {}
 });
