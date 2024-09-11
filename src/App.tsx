d0a73ea84a5f4569f6d012fd89e974ace7228444
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-09-11 14:48:40 +0500

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Provider } from 'react-redux';
 import store from './store';
-import { List } from '../features/todo';
+// import { List } from '../features/todo';
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<List />
+			{/* <List /> */}
 		</Provider>
 	);
 }
