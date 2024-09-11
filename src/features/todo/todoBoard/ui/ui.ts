b4f0e62bf2793c19b458fc64e7e34d50b32b7caa
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-09-11 16:32:39 +0500

    format

--- src/features/todo/todoBoard/ui/ui.ts
+++ src/features/todo/todoBoard/ui/ui.ts
@@ -1,3 +1,3 @@
-export { default as TodoItem } from './TodoItem';
-export { default as TodoList } from './TodoList';
 export { default as TodoBoard } from './TodoBoard';
+export { default as TodoList } from './TodoList';
+export { default as TodoItem } from './TodoItem';
