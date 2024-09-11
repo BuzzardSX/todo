6241c14d63da82b57d705e4cc7178c0e0e849001
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-09-11 17:06:18 +0500

    store

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,18 @@
 import { createElement } from 'react';
+import { Provider } from 'react-redux';
 import { Board } from './features/todo';
+import store from './store';
 
-const App = () => createElement(Board);
+const App = () => createElement(
+	Provider,
+	{
+		children: createElement(
+			'div',
+			null,
+			createElement(Board)
+		),
+		store
+	}
+);
 
 export default App;
