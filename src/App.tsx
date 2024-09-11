35beb3f3fb24d06f523f41061c0fd7165c62dbab
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-09-11 15:10:50 +0500

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux';
 import store from './store';
-// import { List } from '../features/todo';
 import { TodoItem } from './features/todo/ui';
 
 export default function App() {
