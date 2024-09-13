912d45a3e8852b640f1cca918a3e0997f047a2fc
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-09-13 10:05:57 +0500

    selector

--- src/features/todo/board/ui/TodoBoard.ts
+++ src/features/todo/board/ui/TodoBoard.ts
@@ -1,11 +1,14 @@
 import { createElement } from 'react';
+import { useAppSelector } from '../../../../store';
 import { TodoList } from '../ui';
 
 const TodoBoard = () => {
+	const listIds = useAppSelector((state) => state.todoBoard.listIds);
+
 	return createElement(
 		'div',
 		null,
-		[].map(
+		listIds.map(
 			(id) => createElement(TodoList, { id })
 		)
 	)
