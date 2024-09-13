1ff3d2c174c9ef9087f1b833a45fe762f0d5e2d5
Павел Бобров <buzzard.sx@gmail.com>
2024-09-14 01:05:31 +0500

    board

--- src/features/todo/board/ui/TodoBoard.tsx
+++ src/features/todo/board/ui/TodoBoard.tsx
@@ -1,16 +1,15 @@
-import { createElement } from 'react';
 import { useAppSelector } from '../../../../store';
 import { TodoList } from '../ui';
 
 const TodoBoard = () => {
 	const lists = useAppSelector((state) => state.todoBoard.lists);
 
-	return createElement(
-		'div',
-		null,
-		lists.map(
-			({ id }) => createElement(TodoList, { id, key: id })
-		)
+	return (
+		<div>
+			{lists.map(
+				({ id }) => <TodoList id={id} key={id} />
+			)}
+		</div>
 	)
 };
 
