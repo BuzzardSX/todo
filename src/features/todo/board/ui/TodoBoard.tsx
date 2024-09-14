09c50b915581f839a2eedcb4dc465419fbe7803a
Павел Бобров <buzzard.sx@gmail.com>
2024-09-14 13:18:01 +0500

    click

--- src/features/todo/board/ui/TodoBoard.tsx
+++ src/features/todo/board/ui/TodoBoard.tsx
@@ -2,13 +2,16 @@ import { TodoList } from '../ui';
 import { useBoard } from '../hooks';
 
 const TodoBoard = () => {
-	const { board } = useBoard();
+	const { board, addList } = useBoard();
 
 	return (
 		<div>
-			{board.lists.map(
-				({ id }) => <TodoList id={id} key={id} />
-			)}
+			<div>
+				{board.lists.map(
+					({ id }) => <TodoList id={id} key={id} />
+				)}
+			</div>
+			<div onClick={addList}>Click me!</div>
 		</div>
 	);
 };
