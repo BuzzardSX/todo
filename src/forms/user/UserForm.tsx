4a3f323d3e88d908d879417cbce50793bafb2f5b
Павел Бобров <buzzard.sx@gmail.com>
2024-11-09 02:43:47 +0500

    format

--- src/forms/user/UserForm.tsx
+++ src/forms/user/UserForm.tsx
@@ -1,7 +1,11 @@
 import { Form, Input } from 'antd-mobile';
 
 const UserForm = ({ form, initialValues }) => (
-	<Form form={form} initialValues={initialValues} layout='horizontal'>
+	<Form
+		form={form}
+		initialValues={initialValues}
+		layout='horizontal'
+	>
 		<Form.Item label='Age' name='age'>
 			<Input type='number' />
 		</Form.Item>
