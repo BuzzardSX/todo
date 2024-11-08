c17b7c4af346bc9956b3d2c0b8c0d84a28cadef1
Павел Бобров <buzzard.sx@gmail.com>
2024-11-09 02:51:23 +0500

    required

--- src/forms/user/UserForm.tsx
+++ src/forms/user/UserForm.tsx
@@ -5,8 +5,9 @@ const UserForm = ({ form, initialValues }) => (
 		form={form}
 		initialValues={initialValues}
 		layout='horizontal'
+		onFinish={(values) => console.log(values)}
 	>
-		<Form.Item label='Age' name='age'>
+		<Form.Item label='Age' name='age' required>
 			<Input type='number' />
 		</Form.Item>
 	</Form>
