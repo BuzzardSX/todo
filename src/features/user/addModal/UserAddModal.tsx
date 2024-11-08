bd86f0f149643dcea14de250ce65ee30cab890f2
Павел Бобров <buzzard.sx@gmail.com>
2024-11-09 02:08:18 +0500

    format

--- src/features/user/addModal/UserAddModal.tsx
+++ src/features/user/addModal/UserAddModal.tsx
@@ -1,28 +1,26 @@
 import { Modal } from 'antd-mobile';
 
-const UserAddModal = ({ content, onClose, visible }) => {
-	return (
-		<Modal
-			actions={[
-				{
-					key: 'save',
-					text: 'Save',
-					primary: true
-				},
-				{
-					key: 'cancel',
-					text: 'Cancel'
-				}
-			]}
-			closeOnMaskClick
-			content={content}
-			header='Add new user'
-			onAction={onClose}
-			onClose={onClose}
-			showCloseButton
-			visible={visible}
-		/>
-	);
-};
+const UserAddModal = ({ content, onClose, visible }) => (
+	<Modal
+		actions={[
+			{
+				key: 'save',
+				text: 'Save',
+				primary: true
+			},
+			{
+				key: 'cancel',
+				text: 'Cancel'
+			}
+		]}
+		closeOnMaskClick
+		content={content}
+		header='Add new user'
+		onAction={onClose}
+		onClose={onClose}
+		showCloseButton
+		visible={visible}
+	/>
+);
 
 export default UserAddModal;
