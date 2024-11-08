c902a413d52acc0b86614ab030c2bad135ffe403
Павел Бобров <buzzard.sx@gmail.com>
2024-11-08 23:40:39 +0500

    modal

--- src/features/user/addButton/UserAddButton.tsx
+++ src/features/user/addButton/UserAddButton.tsx
@@ -1,30 +1,8 @@
-import { Button, Modal } from 'antd-mobile';
-import { UserForm } from '~/forms';
-import {
-	useFormInitialValues,
-	useModalVisibility
-} from './hooks';
+import { Button } from 'antd-mobile';
 
 const UserAddButton = () => {
-	const [modalVisible, modalShow, modalClose] = useModalVisibility();
-
-	const formInitialValues = useFormInitialValues();
-
 	return (
-		<div>
-			<div>
-				<Button color='primary' onClick={modalShow}>Add user</Button>
-			</div>
-			<div>
-				<Modal
-					closeOnMaskClick
-					content={(
-						<UserForm initialValues={formInitialValues} />
-					)}
-					onClose={modalClose}
-					visible={modalVisible} />
-			</div>
-		</div>
+		<Button color='primary'>Add user</Button>
 	);
 };
 
