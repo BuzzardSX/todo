4a1ca9a977154327d453d75973c6ec79d9f9340c
Павел Бобров <buzzard.sx@gmail.com>
2024-11-08 23:05:14 +0500

    visibility

--- src/features/user/addButton/hooks/userAddButtonHooks.ts
+++ src/features/user/addButton/hooks/userAddButtonHooks.ts
@@ -1,4 +1,34 @@
-import { useMemo } from 'react';
+import { useMemo, useReducer } from 'react';
+
+type State = boolean;
+
+type Show = { type: 'SHOW' };
+
+type Hide = { type: 'HIDE' };
+
+type Action = Show | Hide;
+
+export const userModalVisibility = () => {
+	const [state, dispatch] = useReducer(
+		(state: State, action: Action): State => {
+			switch (action.type) {
+				case 'SHOW':
+					return true;
+				case 'HIDE':
+					return false;
+				default:
+					return state;
+			}
+		},
+		false
+	);
+
+	return [
+		state,
+		() => dispatch({ type: 'SHOW' }),
+		() => dispatch({ type: 'HIDE' })
+	];
+};
 
 export const useFormInitialValues = () => useMemo(
 	() => ({ age: 18 }),
