c8f59e0f5b0d868756fe24d65501f9fe00fb6842
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-11-08 09:25:15 +0500

    form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
 import { Provider } from 'react-redux';
+import { UserForm } from '~/forms';
 import { Board } from '~/features/todo';
 import store from './store';
 
 const App = () => (
 	<Provider store={store}>
+		<div>
+			<UserForm />
+		</div>
 		<Board />
 	</Provider>
 );
