9c13140081fc0f46df151336fa9dc68136a44eab
Бобров Павел Дмитриевич <bobrov@it-limon.ru>
2024-11-08 09:31:22 +0500

    hooks

--- src/forms/user/UserForm.tsx
+++ src/forms/user/UserForm.tsx
@@ -1,5 +1,13 @@
+import { Form, Input } from 'antd-mobile';
+
 const UserForm = () => {
-	return <div>Content</div>;
+	return (
+		<Form>
+			<Form.Item name='age'>
+				<Input />
+			</Form.Item>
+		</Form>
+	);
 };
 
 export default UserForm;
