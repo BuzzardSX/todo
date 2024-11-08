0e423e57f2125a7e2826cf4ba9ac9484e7941327
Павел Бобров <buzzard.sx@gmail.com>
2024-11-09 01:54:42 +0500

    cancel

--- src/features/user/addModal/UserAddModal.tsx
+++ src/features/user/addModal/UserAddModal.tsx
@@ -3,6 +3,12 @@ import { Modal } from 'antd-mobile';
 const UserAddModal = ({ content, onClose, visible }) => {
 	return (
 		<Modal
+			actions={[
+				{
+					key: 'cancel',
+					text: 'Cancel'
+				}
+			]}
 			closeOnMaskClick
 			content={content}
 			header='New user'
