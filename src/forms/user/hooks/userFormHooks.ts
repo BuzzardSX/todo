086af70ec3157651459e94f153812dfe3166f556
Павел Бобров <buzzard.sx@gmail.com>
2024-11-10 12:19:18 +0500

    form

--- src/forms/user/hooks/userFormHooks.ts
+++ src/forms/user/hooks/userFormHooks.ts
@@ -1,4 +1,21 @@
-export const useInitialValues = () => ({
-	name: 'Mike',
-	age: 18
-});
+import { useReducer } from 'react';
+
+export const useInitialValues = () => {
+	const [state, dispatch] = useReducer(
+		(state, action) => {
+			switch (action.type) {
+				default:
+					return state;
+			}
+		},
+		{
+			name: 'Mike',
+			age: 18
+		}
+	);
+
+	return {
+		name: 'Mike',
+		age: 18
+	};
+};
