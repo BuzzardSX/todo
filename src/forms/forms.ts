11948b4b557db16860ed74f1fbe8302009ea6970
Павел Бобров <buzzard.sx@gmail.com>
2024-11-10 15:16:17 +0500

    types

--- src/forms/forms.ts
+++ src/forms/forms.ts
@@ -1,4 +1,16 @@
 import { lazy } from 'react';
+import { Form } from 'antd-mobile';
+
+interface UserFormValues {
+	name: string;
+	age: number;
+}
+
+type UserFormInstance = ReturnType<typeof Form.useForm<UserFormValues>>[0];
+
+export interface UserFormProps {
+	form: UserFormInstance
+}
 
 export const UserForm = lazy(() => import('./user'));
 
