ef738699bb33c0f3da44c3f9460c42d42579b2f9
Павел Бобров <buzzard.sx@gmail.com>
2024-11-24 01:37:48 +0500

    days

--- src/libs/date/Picker.tsx
+++ src/libs/date/Picker.tsx
@@ -1,9 +1,20 @@
+import { useWeek } from './hooks';
 import type { PickerProps } from './date';
 
-const Picker = ({ value }: PickerProps) => (
-	<div className='lib-date-picker-week'>
-		<div>{new Date(value).toString()}</div>
-	</div>
-);
+const Picker = ({ value }: PickerProps) => {
+	const week = useWeek(value);
+
+	return (
+		<div className='date-picker-week'>
+			{week.map(
+				(day) => (
+					<div className='date-picker-day'>
+						{day}
+					</div>
+				)
+			)}
+		</div>
+	);
+};
 
 export default Picker;
