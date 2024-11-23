a6ecab84e38785165e9cfbabcd6fda3aa20d26df
Павел Бобров <buzzard.sx@gmail.com>
2024-11-23 23:36:36 +0500

    week

--- src/libs/date/Picker.tsx
+++ src/libs/date/Picker.tsx
@@ -1,7 +1,7 @@
 import type { PickerProps } from './date';
 
 const Picker = ({ value }: PickerProps) => (
-	<div className='lib-date-picker'>
+	<div className='lib-date-picker-week'>
 		<div>{new Date(value).toString()}</div>
 	</div>
 );
