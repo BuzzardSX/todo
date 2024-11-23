fa4b58bdd0a985812aa12f33bf9f713d65d1c559
Павел Бобров <buzzard.sx@gmail.com>
2024-11-23 21:41:07 +0500

    picker

--- src/libs/date/Picker.tsx
+++ src/libs/date/Picker.tsx
@@ -1,7 +1,7 @@
 import type { PickerProps } from './date';
 
 const Picker = ({ value }: PickerProps) => (
-	<div>{new Date(value).toString()}</div>
+	<div>{new Date(value).getDay()}</div>
 );
 
 export default Picker;
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,14 @@
-const AboutPage = () => <div>About us</div>;
+import { Picker } from '~/libs/date';
+
+const value = new Date(new Date().setDate(3)).getTime();
+
+const AboutPage = () => (
+	<div>
+		<div>
+			<Picker value={value} />
+		</div>
+		<div>About us</div>
+	</div>
+);
 
 export default AboutPage;
