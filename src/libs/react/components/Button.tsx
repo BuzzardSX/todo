0404b24acec92bfe100a59360c3cdf06fcf22f14
Павел Бобров <buzzard.sx@gmail.com>
2025-03-01 20:36:18 +0500

    class

--- src/libs/react/components/Button.tsx
+++ src/libs/react/components/Button.tsx
@@ -4,10 +4,14 @@ import style from './Button.module.css';
 const Button = ({
 	children,
 	type = 'default'
-}: ButtonProps) => (
-	<button className={style.button}>
-		{children}
-	</button>
-);
+}: ButtonProps) => {
+	const className = [style.button, style.primary].join(' ');
+
+	return (
+		<button className={className}>
+			{children}
+		</button>
+	);
+};
 
 export default Button;
