7bd488c8bb3cd3647ac4c941bfa66d1c362c52b0
Павел Бобров <buzzard.sx@gmail.com>
2025-03-01 20:18:03 +0500

    type

--- src/libs/react/components/Button.tsx
+++ src/libs/react/components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonProps } from './reactComponents';
 import style from './Button.module.css';
 
-const Button = ({ children }: ButtonProps) => (
+const Button = ({ children, type = 'default' }: ButtonProps) => (
 	<button className={style.button}>
 		{children}
 	</button>
--- src/libs/react/components/reactComponents.ts
+++ src/libs/react/components/reactComponents.ts
@@ -3,8 +3,11 @@ import type { ReactNode } from 'react';
 export { default as Button } from './Button';
 export { default as Modal } from './Modal';
 
+type ButtonType = 'default' | 'primary';
+
 export interface ButtonProps {
 	children: ReactNode;
+	type: ButtonType;
 }
 
 export interface ModalProps {
