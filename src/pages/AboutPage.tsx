f41cfb61c125228940ae091d60bad9ebe6960255
Павел Бобров <buzzard.sx@gmail.com>
2025-03-01 21:57:46 +0500

    page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,10 @@
+import { Button } from '~/libs/react/components';
+
 const AboutPage = () => (
 	<div>
+		<div>
+			<Button type='primary'>Click me</Button>
+		</div>
 		<div>About us</div>
 	</div>
 );
