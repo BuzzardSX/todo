31f4cf8e3def91dde3656a30e82580bd7e77326c
Павел Бобров <buzzard.sx@gmail.com>
2025-03-25 21:32:04 +0500

    editable

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,22 @@
 import { Button, Text } from '~/libs/react/components';
 
+interface EditableTextProps {
+	content: string;
+}
+
+const EditableText = ({ content }: EditableTextProps) => {
+	return (
+		<div contentEditable />
+	);
+};
+
 const AboutPage = () => (
 	<div>
+		<div>
+			<p contentEditable={true} onInput={() => console.log('!!!')}>
+				Default text.
+			</p>
+		</div>
 		<div>
 			<Button>Default</Button>
 		</div>
