c145e3577b1ec0a3ece067004782e4a7eadab434
Павел Бобров <buzzard.sx@gmail.com>
2025-05-03 01:30:10 +0500

    format

--- src/components/date/picker/DatePickerPopup.tsx
+++ src/components/date/picker/DatePickerPopup.tsx
@@ -3,10 +3,14 @@ import { createPortal } from 'react-dom';
 
 interface DatePickerPopupProps {
 	children: ReactNode;
+	className?: string;
 }
 
-const DatePickerPopup = ({ children }: DatePickerPopupProps) => {
-	return createPortal(<div>{children}</div>, document.body);
+const DatePickerPopup = ({ children, className }: DatePickerPopupProps) => {
+	return createPortal(
+		<div className={className}>{children}</div>,
+		document.body
+	);
 };
 
 export default DatePickerPopup;
