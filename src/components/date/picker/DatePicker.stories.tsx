be5aec5c3ae6ebbaa618bb6985e859d08bfb3655
Павел Бобров <buzzard.sx@gmail.com>
2025-05-02 21:12:58 +0500

    story

--- src/components/date/picker/DatePicker.stories.tsx
+++ src/components/date/picker/DatePicker.stories.tsx
@@ -1,8 +1,16 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import DatePicker from './DatePicker';
 
 const meta: Meta<typeof DatePicker> = {
 	component: DatePicker
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const antLike: Story = {
+	args: {
+		placeholder: 'Select date'
+	}
 };
 
 export default meta;
