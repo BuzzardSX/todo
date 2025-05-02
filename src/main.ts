3ed1c5c4b8e8627f27c821d1c29c19e837b792f7
Павел Бобров <buzzard.sx@gmail.com>
2025-05-02 20:36:35 +0500

    themes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
-import './themes/github.css';
 
 const app = document.getElementById('app');
 
