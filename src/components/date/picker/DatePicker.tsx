cb3f29b9fa2dc81654a26518cfcd7cfef4762a82
Павел Бобров <buzzard.sx@gmail.com>
2025-05-02 22:38:06 +0500

    format

--- src/components/date/picker/DatePicker.tsx
+++ src/components/date/picker/DatePicker.tsx
@@ -4,12 +4,18 @@ import style from './DatePicker.module.css';
 type DatePickerSize = 'large' | 'middle' | 'small';
 
 interface DatePickerProps {
+	className?: string;
 	placeholder?: string;
 	size?: DatePickerSize;
 	suffix?: ReactNode;
 }
 
-const DatePicker = ({ placeholder, size = 'middle', suffix }: DatePickerProps) => (
+const DatePicker = ({
+	className,
+	placeholder,
+	size = 'middle',
+	suffix = null
+}: DatePickerProps) => (
 	<input className={style.root} placeholder={placeholder} />
 );
 
