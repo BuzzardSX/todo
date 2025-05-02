48a9085f202c34086e9209afce1f67f712a55662
Павел Бобров <buzzard.sx@gmail.com>
2025-05-02 20:47:42 +0500

    page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,22 +1,8 @@
-import { EditableText, Text } from '~/libs/react/components';
-import Button from '~/libs/react/components/Button';
-import HeadOutlet from '~/libs/react/components/HeadOutlet';
+import DatePicker from '~/components/date/picker'
 
 const AboutPage = () => (
 	<div>
-		<HeadOutlet>
-			WTF
-		</HeadOutlet>
-		<div>
-			<EditableText />
-		</div>
-		<div>
-			<Button>Default</Button>
-		</div>
-		<div>
-			<Button type='primary'>Primary</Button>
-		</div>
-		<Text>About us</Text>
+		<DatePicker />
 	</div>
 );
 
