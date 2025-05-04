3555e6e74b45d5b1f887067ad4ed25327c774474
Павел Бобров <buzzard.sx@gmail.com>
2025-05-04 14:15:33 +0500

    month

--- src/components/date/picker/ui/DatePickerPanel.tsx
+++ src/components/date/picker/ui/DatePickerPanel.tsx
@@ -1,12 +1,17 @@
+const month = [1, 2, 4, 5];
 const week = [1, 2, 3, 4, 5, 6, 7];
 
 const DatePickerPanel = () => {
 	return (
-		<tr>
-			{week.map((n) => (
-				<td>{n}</td>
+		<table>
+			{month.map(() => (
+				<tr>
+					{week.map((n) => (
+						<td>{n}</td>
+					))}
+				</tr>
 			))}
-		</tr>
+		</table>
 	);
 };
 
