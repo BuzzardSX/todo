faaaa44eeddd01c9d0554fe9f34b34635fbf7367
Павел Бобров <buzzard.sx@gmail.com>
2025-05-05 02:30:49 +0500

    index

--- src/libs/react/components/index.ts
+++ src/libs/react/components/index.ts
@@ -1,4 +1,4 @@
-export { EditableText, Theme } from './reactComponents';
+export { Theme } from './reactComponents';
 
 export type {
 	ButtonProps,
--- src/libs/react/components/reactComponents.ts
+++ src/libs/react/components/reactComponents.ts
@@ -1,2 +1,1 @@
-export { default as EditableText } from './EditableText';
 export { default as Theme } from './Theme';
