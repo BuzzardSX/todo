a122938cf339da8deeb6093c19dad68f154275fc
Павел Бобров <buzzard.sx@gmail.com>
2025-05-06 20:10:39 +0500

    week

--- src/components/calendarRow/CalendarRow.tsx
+++ src/components/calendarRow/CalendarRow.tsx
@@ -1,12 +1,15 @@
 import CalendarCell from '~/components/calendarCell';
 import style from './CalendarRow.module.css';
 
-const week = [1, 2, 3, 4, 5, 6, 7];
-
 const CalendarRow = () => (
 	<tr className={style.root}>
-		{week.map(() => <CalendarCell />)}
+		<CalendarCell />
+		<CalendarCell />
+		<CalendarCell />
 		<CalendarCell today />
+		<CalendarCell />
+		<CalendarCell />
+		<CalendarCell />
 	</tr>
 );
 
