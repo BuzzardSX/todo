a6d4df439d044fc43f7cbc5cd08b371b78ce8824
Павел Бобров <buzzard.sx@gmail.com>
2025-05-06 20:25:43 +0500

    format

--- src/libs/react/components/Modal.tsx
+++ src/libs/react/components/Modal.tsx
@@ -2,15 +2,15 @@ import cx from 'classnames';
 import type { ReactNode } from 'react';
 import style from './modal.module.css';
 
+interface ModalProps {
+	children?: ReactNode;
+	open: boolean;
+}
+
 const Modal = ({ children, open }: ModalProps) => (
 	<div className={cx(style.root, { [style.open]: open })}>
 		{children}
 	</div>
 );
-
-interface ModalProps {
-	children?: ReactNode;
-	open: boolean;
-}
 
 export default Modal;
