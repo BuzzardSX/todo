b1827f875a725af25551bdc2ca6aea7a7ff23f7b
Павел Бобров <buzzard.sx@gmail.com>
2025-05-06 20:30:32 +0500

    format

--- src/libs/react/components/Theme.tsx
+++ src/libs/react/components/Theme.tsx
@@ -1,13 +1,13 @@
 import type { CSSProperties, ReactNode } from 'react';
 import style from './theme.module.css';
 
-const Theme = ({ children, ...props }: ThemeProps) => (
-	<div className={style.root} style={props.style}>{children}</div>
-)
-
-export interface ThemeProps {
+interface ThemeProps {
 	children: ReactNode;
 	style?: CSSProperties;
 }
+
+const Theme = ({ children, ...props }: ThemeProps) => (
+	<div className={style.root} style={props.style}>{children}</div>
+)
 
 export default Theme;
