d5e5644da147016aa831d7291e83b122b02a9e56
Павел Бобров <buzzard.sx@gmail.com>
2025-05-08 01:51:47 +0500

    suspense

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,13 +1,22 @@
+import { Suspense } from 'react';
 import style from './Calendar.module.css';
 import { CalendarRow } from './components';
 
 const Calendar = () => (
 	<div className={style.root}>
 		<table className={style.table}>
-			<CalendarRow />
-			<CalendarRow />
-			<CalendarRow />
-			<CalendarRow />
+			<Suspense fallback={null}>
+				<CalendarRow />
+			</Suspense>
+			<Suspense fallback={null}>
+				<CalendarRow />
+			</Suspense>
+			<Suspense fallback={null}>
+				<CalendarRow />
+			</Suspense>
+			<Suspense fallback={null}>
+				<CalendarRow />
+			</Suspense>
 		</table>
 	</div>
 );
