a9f10a73e1af30e8ff871977a9f87dde8c85c31a
Павел Бобров <buzzard.sx@gmail.com>
2025-05-08 00:38:48 +0500

    story

---
+++ src/components/Calendar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Calendar from './Calendar';
+import Theme from './Theme';
+
+const meta: Meta<typeof Calendar> = {
+	component: Calendar,
+	decorators: (Story) => (
+		<Theme>
+			<div style={{ padding: '1em' }}>
+				<Story />
+			</div>
+		</Theme>
+	)
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const basic: Story = {};
+
+export default meta;
