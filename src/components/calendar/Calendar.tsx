1fcf48fa960c1422de720aac68facc6c61c096aa
Павел Бобров <buzzard.sx@gmail.com>
2025-05-07 23:21:50 +0500

    month

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,12 +1,13 @@
 import CalendarRow from '~/components/calendarRow';
 import style from './Calendar.module.css';
 
-const month = [1, 2, 4, 5];
-
 const Calendar = () => (
 	<div className={style.root}>
 		<table>
-			{month.map(() => <CalendarRow />)}
+			<CalendarRow />
+			<CalendarRow />
+			<CalendarRow />
+			<CalendarRow />
 		</table>
 	</div>
 );
